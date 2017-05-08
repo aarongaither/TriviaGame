62d45a058948ce0f6a15f13a80984f14080d088f
Aaron Gaither <AaronGaither@coresystems.local>
2017-05-08 00:52:33 -0700

    convert to object structure, add html for timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,69 +1,65 @@
 function makeHTML() {
     $('#questionRow').append($('<div>').addClass('question').attr('id', 'question').text('question goes here'));
     for (let i = 0; i < 4; i++) {
-        let answerDiv = $('<div>').addClass('answer').attr('id', i).text('answer ' + i);
-        answerDiv.click(function() { checkAnswer(this.id); });
+        let answerDiv = $('<div>').addClass('answer').attr('id', i).text('answer ' + i)
+            .click(function() { go.checkAnswer(this.id); });
         $('#answerRow').append(answerDiv);
     }
 }
 
-function getRandom(max) {
-    return Math.floor(Math.random() * max);
-}
+go = {
+    qUsed: [],
+    numberCorrect: 0,
+    numberWrong: 0,
+    newQuestion: function() {
 
-function newQuestion() {
+        function getQuestion() {
 
-    function getQuestion() {
-
-        function arrShuff(arr) {
-            tempArr = []
-            let cnt = arr.length;
-            for (let i = 0; i < cnt; i++) {
-                let x = getRandom(arr.length);
-                let item = arr.splice(x, 1);
-                tempArr.push(item[0]);
+            function arrShuff(arr) {
+                tempArr = []
+                let cnt = arr.length;
+                for (let i = 0; i < cnt; i++) {
+                    let x = go.getRandom(arr.length);
+                    let item = arr.splice(x, 1);
+                    tempArr.push(item[0]);
+                }
+                return tempArr;
             }
-            return tempArr;
+            let rand = go.getRandom(qList.length);
+            let qToReturn = qList.splice(rand, 1)[0];
+            qToReturn.answers = arrShuff(qToReturn.answers);
+            return qToReturn;
         }
-        let rand = getRandom(qList.length);
-        let qToReturn = qList.splice(rand, 1)[0];
-        qToReturn.answers = arrShuff(qToReturn.answers);
-        return qToReturn;
-    }
 
-    function updatePage(q) {
-        $('#question').text(q.q);
-        for (let i = 0; i < q.answers.length; i++) {
-            $("#" + i).text(q.answers[i]);
+        function updatePage(quesObj) {
+            $('#question').text(quesObj.q);
+            for (let i = 0; i < quesObj.answers.length; i++) {
+                $("#" + i).text(quesObj.answers[i]);
+            }
         }
+
+        this.curQ = getQuestion();
+        updatePage(this.curQ);
+
+    },
+    checkAnswer: function(num) {
+        ans = this.curQ.answers[num]
+        if (ans === this.curQ.correctAnswer) {
+            this.numberCorrect += 1;
+        } else {
+            this.numberWrong += 1;
+        }
+        this.qUsed.push(this.curQ);
+        if (this.numberCorrect + this.numberWrong < 10) {
+            this.newQuestion();
+        } else {
+            console.log("Game Over. Number Correct: " + this.numberCorrect + ", Number Wrong: " + this.numberWrong)
+        }
+    },
+    getRandom: function (max) {
+        return Math.floor(Math.random() * max);
     }
-
-    curQ = getQuestion();
-    if (curQ !== undefined){
-    	updatePage(curQ);
-    }
-
-}
-
-let curQ;
-let numberCorrect = 0;
-let numberWrong = 0;
-
-function checkAnswer(num) {
-    ans = curQ.answers[num]
-    if (ans === curQ.correctAnswer) {
-        numberCorrect += 1;
-    } else {
-        numberWrong += 1;
-    }
-
-    if (numberCorrect + numberWrong < 10) {
-	    newQuestion();
-    } else {
-    	console.log("Game Over. Number Correct: " + numberCorrect + ", Number Wrong: " + numberWrong)
-    }
-
 }
 
 makeHTML();
-newQuestion();
+go.newQuestion();
